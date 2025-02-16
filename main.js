fd865cbb59dbf0e8c4b0dd9c6e9fea708983598d
Jawne3 <johnlowendavid@yahoo.ca>
2025-02-16 16:40:43 -0700

    created an array of players and games, added getter methods for them

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
 const team = {
-    
-}
+    _players: [{
+        firstName: 'John', lastName: 'Doe', age: 23}, {
+        firstName: 'Sarah', lastName: 'Poop', age: 22}, {
+        firstName: 'Hello', lastName: 'Name', age: 19}
+        ],
+    _games: [{
+        opponent: 'eagles', teamPoints: 54, opponentPoints: 5}, {
+        opponent: 'snakes', teamPoints: 60, opponentPoints: 34}, {
+        opponent: 'dragons', teamPoints: 56, opponentPoints: 63}
+    ],
+    get games() {
+        return this._players;
+    },
+    get players() {
+        return this._players;
+    }
+
+};
