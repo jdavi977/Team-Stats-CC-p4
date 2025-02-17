8766f0b10098ec27cebf77361dd1423240294ed5
Jawne3 <johnlowendavid@yahoo.ca>
2025-02-16 17:00:18 -0700

    Created addplayer and addgame methods

--- main.js
+++ main.js
@@ -14,6 +14,18 @@ const team = {
     },
     get players() {
         return this._players;
+    },
+    addPlayer(newFirstName, newLastName, newAge) {
+        const player = {firstName: newFirstName, lastName: newLastName, age: newAge}
+        team._players.push(player);
+    },
+    addGame(newOpponent, newTeamPoints, newOpponentPoints) {
+        const game = {opponent: newOpponent, teamPoints: newTeamPoints, newOpponent: newOpponentPoints}
+        team._games.push(game);
     }
-
 };
+team.addPlayer('Bugs', 'Bunny', 76);
+console.log(team._players);
+
+team.addGame('Titans', 100, 98);
+console.log(team._games);
